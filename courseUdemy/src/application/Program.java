d776244c19db44592ac3b955d8f098ab62e84522
gabizzzs <glopes.p09@gmail.com>
2022-12-06 19:30:46 -0300

    exercicios udemy sobre oop

--- courseUdemy/src/application/Program.java
+++ courseUdemy/src/application/Program.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 import entities.Rectangle;
 
 public class Program {
+	public static void main(String[] args) {
 
-	Scanner sc = new Scanner(System.in);
-	
-	Rectangle rect = new Rectangle();
-	
-	System.out.println("Enter rectangle width ande height: ");
-	
+		Scanner sc = new Scanner(System.in);
+		
+		Rectangle rect = new Rectangle();
+		
+		System.out.println("Enter rectangle width ande height: ");
+		rect.width = sc.nextDouble();
+		rect.height = sc.nextDouble();
+		
+		System.out.println("Area = %.2d%n", rect.area());
+		System.out.println("Perimeter = %.2f%n", rect.perimeter());
+	}
 }
